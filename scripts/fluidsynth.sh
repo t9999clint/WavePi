62869d5eba4517828bc36b0324952a03e84743ea
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-06-19 06:38:05 +0000

    Update fluidsynth.sh

--- scripts/fluidsynth.sh
+++ scripts/fluidsynth.sh
@@ -2,9 +2,6 @@
 ## fluidsynth.sh synth script example
 ## This script is meant to be run with the following syntax fluidsynth.sh <sf2 file> <alsa device number>
 
-## priority of the fluidsynth program. (-20 to 19, lower is better)
-FLUID_PRIORITY = -18
-
 ## set of reverb preferences to more closely match soundblaster awe
 fluid_chorus_type=1
 fluid_chorus_depth=8
@@ -20,14 +17,14 @@ fluid_interp=1
 ##find how many cpu threads are availible and act accordingly
 CPU_LIMIT=$(nproc)
 if (( CPU_LIMIT > 1)); then
-    CPU_LIMIT = $((CPU_LIMIT - 1))
+    CPU_LIMIT=$((CPU_LIMIT - 1))
 fi
-POLY_LIMIT = $((CPU_LIMIT * 75))
+POLY_LIMIT=$((CPU_LIMIT * 75))
 
 ## sanity check for poly limit (max of 999)
 if (( POLY_LIMIT > 999)); then
-    POLY_LIMIT = 999
+    POLY_LIMIT=999
 fi
 
-## launch fluiddynth with set priority
-nice -n $FLUID_PRIORITY /usr/local/bin/fluidsynth -o synth.cpu-cores=$CPU_LIMIT -o audio.alsa.device=hw:$2 -o audio.period-size=64 -o synth.polyphony=$POLY_LIMIT -R 0 -C 0 -is -r 44100 --gain 0.6 --audio-driver=alsa "$1"
+## launch fluiddynth
+fluidsynth -o synth.cpu-cores=$CPU_LIMIT -o audio.alsa.device=hw:$2 -o audio.period-size=64 -o synth.polyphony=$POLY_LIMIT -R 0 -C 0 -is -r 44100 --gain 0.6 --audio-driver=alsa "$1"
