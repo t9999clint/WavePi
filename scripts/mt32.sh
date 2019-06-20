d136773cd1dbda4652a00a3863ca4feba3bd3f12
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-06-20 09:24:05 +0000

    Update mt32.sh

--- scripts/mt32.sh
+++ scripts/mt32.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ##launch mt32
 CURRENT_DIR=$(dirname $(readlink -f $0))
-LD_PRELOAD=/usr/local/lib/libmt32emu.so.2 mt32d -o 2 -m -i 24 -f "$CURRENT_DIR/../roms/"
+LD_PRELOAD=/usr/local/lib/libmt32emu.so.2 mt32d -o 2 -i 24 -f "$CURRENT_DIR/../roms/"
