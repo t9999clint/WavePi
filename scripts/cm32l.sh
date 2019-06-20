28a338f4a0b53bb65055dc915aa432ea4d735e88
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-06-20 09:23:45 +0000

    Update cm32l.sh

--- scripts/cm32l.sh
+++ scripts/cm32l.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 ##launch cm32l
-LD_PRELOAD=/usr/local/lib/libmt32emu.so.2 /usr/local/bin/mt32d -o 1 -i 24 -f ../roms/
+CURRENT_DIR=$(dirname $(readlink -f $0))
+LD_PRELOAD=/usr/local/lib/libmt32emu.so.2 mt32d -o 1 -i 24 -f "$CURRENT_DIR/../roms/"
