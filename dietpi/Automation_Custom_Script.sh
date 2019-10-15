a89f81157d9f35bd56faddb70b439a4a97e6749e
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-10-15 02:49:16 +0000

    fixing a typo

--- dietpi/Automation_Custom_Script.sh
+++ dietpi/Automation_Custom_Script.sh
@@ -5,7 +5,7 @@ sudo apt-get install wget -y
 sudo apt-get install nano -y
 sudo apt-get install tar -y
 
-cd \home\dietpi
+cd /home/dietpi
 wget https://github.com/t9999clint/WavePi/archive/v0.8.0.tar.gz
 tar -xzf v0.8.0.tar.gz
 mv WavePi-0.8.0 WavePi
