c40fcc3fb221778f57e5f6bb2284a6e822333345
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-10-15 02:47:30 +0000

    Update Automation_Custom_Script.sh

--- dietpi/Automation_Custom_Script.sh
+++ dietpi/Automation_Custom_Script.sh
@@ -7,7 +7,7 @@ sudo apt-get install tar -y
 
 cd \home\dietpi
 wget https://github.com/t9999clint/WavePi/archive/v0.8.0.tar.gz
-tar -xzf WavePi-WavePi.tar.gz
+tar -xzf v0.8.0.tar.gz
 mv WavePi-0.8.0 WavePi
 cd WavePi
 chmod +x ./install.sh
