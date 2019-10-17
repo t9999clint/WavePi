2c5fb68ca301b290888210ac8d8e1e38c4626935
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-10-17 13:45:30 +0000

    slight modification to script
    still haven't tested this yet, but it should work better this time. I still need to change the internet delay after the setup is done, but I'm not sure how to do that yet.

--- dietpi/Automation_Custom_Script.sh
+++ dietpi/Automation_Custom_Script.sh
@@ -11,8 +11,9 @@ tar -xzf v0.8.0.tar.gz
 mv WavePi-0.8.0 WavePi
 cd WavePi
 chmod +x ./install.sh
-./install.sh
-cd ..
-cd dietpi
 sudo rm /etc/proftpd/proftpd.conf
-sudo cp proftpd.conf /etc/proftpd/proftpd.conf
+sudo cp /home/dietpi/WavePi/dietpi/proftpd.conf /etc/proftpd/proftpd.conf
+service proftpd restart
+./install.sh
+
+echo "DietPi autoconfiguration is finished, please reboot"
