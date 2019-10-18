8099e0e76ea407aaeca911af3cf4b3a74b37f602
t9999clint <50733691+t9999clint@users.noreply.github.com>
2019-10-18 02:40:29 +0000

    fixing auto install issues

--- dietpi/Automation_Custom_Script.sh
+++ dietpi/Automation_Custom_Script.sh
@@ -8,6 +8,7 @@ sudo apt-get install tar -y
 cd /home/dietpi
 wget https://github.com/t9999clint/WavePi/archive/v0.8.0.tar.gz
 tar -xzf v0.8.0.tar.gz
+rm v0.8.0.tar.gz
 mv WavePi-0.8.0 WavePi
 cd WavePi
 
@@ -15,7 +16,7 @@ chmod +x ./install.sh
 sudo rm /etc/proftpd/proftpd.conf
 sudo cp /home/dietpi/WavePi/dietpi/proftpd.conf /etc/proftpd/proftpd.conf
 service proftpd restart
+sudo usermod -a -G audio dietpi
+sudo usermod -a -G i2c dietpi
 ./install.sh
 sudo chown -R dietpi /home/dietpi/WavePi
-
-echo "DietPi autoconfiguration is finished, please reboot"
